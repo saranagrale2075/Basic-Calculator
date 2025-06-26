be30eef1022d199593f35138be542bbc4d4587a6
Sara Anil Nagrale. <saranagrale42@gmail.com>
2025-06-26 12:37:15 +0530

    Files Added

---
+++ script.js
@@ -0,0 +1,25 @@
+function appendValue(value) {
+    document.getElementById("display").
+    value +=value;
+}
+
+function clearDisplay(){
+    document.getElementById("display").
+    value ="";
+
+}
+function backspace(){
+    let display = document.getElementById("display");
+    display.value = display.value.slice(0,-1);
+}
+
+function calculateResult(){
+    try {
+        document.getElementById("display").
+        value = eval(document.getElementById("display").value);
+
+    } catch {
+        document.getElementById("display").
+        value = "Error";
+    }
+}
